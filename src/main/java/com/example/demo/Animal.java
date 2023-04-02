111c6350fa43b0dbdc12c33e08d0d7b066636727
CadanGlass <113327002+CadanGlass@users.noreply.github.com>
2023-04-02 12:49:50 -0700

    Add javadocs for animal class

--- src/main/java/com/example/demo/Animal.java
+++ src/main/java/com/example/demo/Animal.java
@@ -1,11 +1,25 @@
 package com.example.demo;
 
+/**
+ * The Animal class represents an animal that can be purchased and produces a product.
+ *
+ * @author Annette Lam, Cadan Glass
+ * @version 2023
+ */
 public class Animal {
     private String type;
     private int cost;
     private String productType;
     private String imagePath;
 
+    /**
+     * Constructs an Animal object with the specified type, product type, cost, and image path.
+     *
+     * @param type        the type of animal
+     * @param productType the product type that the animal provides
+     * @param cost        the cost of the animal
+     * @param imagePath   the file path of the image for the animal
+     */
     public Animal(String type, String productType, int cost, String imagePath) {
         this.type = type;
         this.productType = productType;
@@ -25,12 +39,21 @@ public class Animal {
         return productType;
     }
 
+    /**
+     * Returns the string representation of the Animal object, which is the type of animal.
+     *
+     * @return the string representation of the Animal object
+     */
     @Override
     public String toString() {
         return type;
     }
 
-    // Getter and setter for imagePath
+    /**
+     * Returns the file path of the image for the animal.
+     *
+     * @return the file path of the image for the animal
+     */
     public String getImagePath() {
         return imagePath;
     }
